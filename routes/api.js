a0c839869067a0281a400c994ba07aae1b426946
ChristianAlteri <alteri.christian@gmail.com>
2023-05-31 17:54:16 +0800

    notes bug

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const dbFilePath = path.join(__dirname, 'db', 'db.json');
+const uuid = require('uuid');
+
+const dbPath = path.join(__dirname, '..', 'db', 'db.json');
 
 const router = express.Router();
-const app = express();
 
 
-router.get('/', (req, res) => {
+
+router.get('/notes', (req, res) => {
     try {
-        const data = fs.readFileSync(dbFilePath, 'utf8');
-        const jsonData = JSON.parse(data);
-        res.json(jsonData)
+        const data = fs.readFileSync(dbPath, 'utf8');
+        res.json(data)
     } catch (error) {
         res.status(500).send('Internal Server Error');  
     }
